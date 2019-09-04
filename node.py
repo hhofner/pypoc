1211a819beaa121e5e9eedecab261a12788f61f1
Hans Hofner <hhofner@gmail.com>
2019-09-04 13:43:05 +0900

    add kwargs support

--- node.py
+++ node.py
@@ -1,9 +1,10 @@
 import simpy
 import logging
+from exceptions import RunMethodEmpty
 
 logging.basicConfig(filename="node.log", level=logging.INFO, filemode="w")
 
-class Node:
+class Node(object):
     current_id = 0
     @staticmethod
     def get_id():
@@ -11,22 +12,50 @@ class Node:
         Node.current_id += 1
         return id
 
-    def __init__(self, env):
+    def __init__(self, **kwargs):
         self.id = Node.get_id()
-        self.env = env
-        try:
-            self.action = env.process(self.run())
-        except ValueError as err:
-            print(f"Node {self.id}\'s run() method must be overwritten!\nError message: {err}")
-
         logging.info(f"Instantiating Node {self.id}")
-        self.x = None
-        self.y = None
-        self.z = None
+        self.x = kwargs['x']
+        self.y = kwargs['y']
+        self.z = kwargs['z']
+
+        if 'env' in kwargs.keys():
+            self.env = kwargs['env']
+            try:
+                self.action = self.env.process(self.run())
+            except ValueError as err:
+                raise RunMethodEmpty()
+
+    def add_env(self, env):
+        if self.env:
+            logging.info("Warning: Overwriting env variable")
+            self.env = env
+        else:
+            self.env = env
+            try:
+                self.action = self.env.process(self.run())
+            except RunMethodEmpty as err:
+                raise RunMethodEmpty
 
     def run(self):
         pass
 
+    def __hash__(self):
+        # Returns the hashed representation of the string
+        return hash(repr(self))
+
+class MovingNode(Node):
+    """ Simple node object with mobility model """
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.mobility_model = kwargs['mobility_model']
+
+    def run(self):
+        #TODO: Error case 
+        while True:
+            print(f'Node {self.id} at {self.x, self.y, self.z}')
+            yield self.env.timeout(1)
+            self.x,self.y,self.z = self.mobility_model(self.x, self.y, self.z)
 
 if __name__ == "__main__":
     pass
