5e1a86311eb48b9d7711fd3b1435931939cab4e9
Hans Hofner <hhofner@gmail.com>
2019-09-04 13:42:34 +0900

    remove properties and add run method

--- network.py
+++ network.py
@@ -1,31 +1,49 @@
+import simpy
+import networkx as nx
 import node
+import numpy as np
 
 class Network:
-    def __init__(self):
-        self.nodes      = []
-        self.outerNodes = []
-        self.UAVs       = [] #todo is this a good design?
-        self.cloudlets  = []
-        self.users      = []
-        self.venues     = []
+    def __init__(self, ebunch, env_time, **kwargs):
+        self.env = simpy.Environment()
+        self.env_time = env_time
+        self.nodes = []
+        # for bunch in ebunch:
+        #     node1, node2, weight = bunch
+        #     node1.add_env(self.env)
+        #     node2.add_env(self.env)
 
-        self.cloud
-        # priority_queue<Node*, vector<Node*>, Compare> m_priNodes;
-        # priority_queue<Node*, vector<Node*>, CompareRandom> m_priNodesRandom;
-        # Graph * m_conGraph; // graph of channel assignment
-        # DGraph * m_dGraph; // graph of routing
-        # property_map < Graph, vertex_index_t >::type
-        # node_index = get(vertex_index, *m_conGraph);
-        # property_map < Graph, edge_index_t >::type
-        # edges_index = get(edge_index, *m_conGraph);
-        # property_map < DGraph, edge_index_t >::type
-        # edges_index_d = get(edge_index, *m_dGraph);
-        # property_map < Graph, edge_weight_t >::type
-        # edges_weight_channel = get(edge_weight, *m_conGraph);
-        # property_map < DGraph, edge_weight_t >::type
-        # edges_weight_load = get(edge_weight, *m_dGraph);
+        # G = nx.Graph()
+        # G.add_edges_from(ebunch)
 
-        self.inData
-        self.inDataSize
-        self.cuTime
+    def run(self):
+        pass
 
+    def start(self):
+        print("Starting network")
+        self.env.run(until=self.env_time)
+
+class HelloNetwork(Network):
+    def __init__(self, **kwargs):
+        self.env_time = 15
+        self.ebunch = [()]
+        super().__init__(self.ebunch, self.env_time, **kwargs)
+        self.nodes = []
+
+        self.kwargs = {'x':0,
+                       'y':0,
+                       'z':0,
+                       'mobility_model': lambda x,y,z: (x+1, y+1, z+1),
+                       'env': self.env,}
+
+        for _ in range(2):
+            kwargs = dict(self.kwargs)
+            kwargs['x'] = np.random.randint(5)
+            kwargs['y'] = np.random.randint(5)
+            kwargs['z'] = np.random.randint(5)
+            self.nodes.append(node.MovingNode(**kwargs))
+
+if __name__ == "__main__":
+    print("Starting test network")
+    hello_network = HelloNetwork()
+    hello_network.start()
