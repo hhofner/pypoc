0c864ae27506b9d4dfa44b1e358635ad31acbe65
Hans Hofner <hhofner@gmail.com>
2019-09-16 17:48:27 +0900

    stuff

--- simple_node.py
+++ simple_node.py
@@ -1,5 +1,6 @@
 import packet
 import numpy as np
+import generation_models
 
 class Node(object):
     current_id = 0
@@ -13,12 +14,16 @@ class Node(object):
     def __init__(self, **kwargs):
         self.id    = Node.get_id()
         self.queue = []
+        self.time  = 0
+        self.dropped_packets = [] #TODO: Better implementation pls
 
         self.config = {'generation_rate': 0,
                        'destinations': [],
                        'type': 'node',
-                       'serv_rate': 1,
-                       'queue_cap': 4}
+                       'serv_rate': 10,
+                       'queue_cap': 350,
+                       'gen_scheme': 'linear',}
+
         self.update(**kwargs)
 
     def update(self, **kwargs):
@@ -28,43 +33,45 @@ class Node(object):
                     self.config[key] = kwargs[key]
                 else:
                     #TODO: Logging
-                    pass
-
-        self.generation_rate = self.config['generation_rate']
-        self.destinations = self.config['destinations']
-        self.type = self.config['type']
-        self.serv_rate = self.config['serv_rate']
-        self.queue_cap = self.config['queue_cap']
+                    raise Exception(f'kwargs[{key}] is not of type {self.config[key]}')
 
     def transmit(self):
         self.generate_packets()
         return self.service()
 
     def receive(self, packet):
+        try:
+            queue_cap = self.config['queue_cap']
+        except KeyError as err:
+            raise err
+
         packet.source = self.id
         if packet.destination == self.id:
             print(f'Node {self.id} received packet {packet.id}')
         else:
-            if not len(self.queue) > self.queue_cap:
+            if not len(self.queue) >= queue_cap:
                 self.queue.append(packet)
             else:
-                print(f'Packet {packet.id} dropped at node {self.id}')
+                self.dropped_packets.append(packet)
 
     def generate_packets(self):
-        if self.generation_rate <= 0:
+        generation_rate = self.config['generation_rate']
+        destinations = self.config['destinations']
+        if generation_rate <= 0:
             return None
         else:
-            for _ in range(self.generation_rate):
-                if not self.destinations:
+            for _ in range(generation_rate):
+                if not destinations:
                     return None
                 else:
-                    rand_int = np.random.randint(len(self.destinations))
-                    self.queue.append(packet.Packet(self.id, self.destinations[rand_int]))
+                    rand_int = np.random.randint(len(destinations))
+                    self.queue.append(packet.Packet(self.id, destinations[rand_int]))
 
     def service(self):
         packets_to_send = []
+        serv_rate = self.config['serv_rate']
         if self.queue:
-            for _ in range(self.serv_rate):
+            for _ in range(serv_rate):
                 try:
                     packets_to_send.append(self.queue.pop())
                 except IndexError:
@@ -76,3 +83,20 @@ class Node(object):
 
     def __hash__(self):
         return hash(self.id)
+
+class VaryingPacketGeneratingNode(Node):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def update(self, **kwargs):
+        super().update(**kwargs)
+
+    def generate_packets(self):
+        if self.config['gen_scheme'] == 'linear':
+            gen_func = generation_models.basic_linear_generation
+        elif self.config['gen_scheme'] == 'basic_normal':
+            gen_func = generation_models.basic_normal_generation
+        elif self.config['gen_scheme'] == 'time_experimental':
+            gen_func = generation_models.time_experimental_generation
+
+        gen_func(self)
