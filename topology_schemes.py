7c4d99a86ec3a1f788ed91fa82719178d7a74d6b
Hans Hofner <hhofner@gmail.com>
2019-09-24 17:16:07 +0900

    add new topology scheme

--- topology_schemes.py
+++ topology_schemes.py
@@ -1,4 +1,6 @@
 import simple_node
+import numpy as np
+import channel
 
 def default_sagin(ground_node_count=8, air_node_count=10, space_node_count=4):
     nodes = []
@@ -13,6 +15,7 @@ def default_sagin(ground_node_count=8, air_node_count=10, space_node_count=4):
     gnc = ground_node_count
     anc = air_node_count
     for gn in ground_nodes[:int(gnc / 2)]:
+        gn.update(**{'serv_rate': int(np.random.normal(100, 50))})
         for an in air_nodes[:int(anc / 2)]:
             ebunch.append((gn.id, an.id, {'Weight': 1}))
 
@@ -36,16 +39,37 @@ def default_sagin(ground_node_count=8, air_node_count=10, space_node_count=4):
     destinations = [n.id for n in ground_nodes[int(gnc / 2):]]
     for tgn in ground_nodes[:int(gnc / 2)]:
         tgn.update(**{'destinations': destinations})
-        tgn.update(**{'generation_rate': int(np.random.normal(100, 50))})
+        tgn.update(**{'generation_rate': 1})
 
-    # update relaying nodes
+    # update serve rates
     for an in air_nodes:
-        an.update(**{'serv_rate': 4})
+        an.update(**{'serv_rate': int(np.random.normal(30, 10))})
     for sn in sat_nodes:
-        sn.update(**{'serv_rate': 2})
+        sn.update(**{'serv_rate': int(np.random.normal(30, 10))})
 
     nodes += ground_nodes
     nodes += air_nodes
     nodes += sat_nodes
 
     return (ebunch, nodes)
+
+def test_topology(node_count=2):
+    '''
+    Test topology building method.
+    1. Create nodes
+    2. Create their connections in form of ebunches.
+    3. Return the ebunch & network
+
+    :param node_count:
+    :return:
+    '''
+    transmit_nodes = []
+    receive_nodes  = []
+
+    num_of_transmit = node_count//2
+    num_of_receive  = node_count - num_of_transmit
+
+    for _ in range(num_of_receive):
+        receive_nodes.append(simple_node.Node())
+    for _ in range(num_of_transmit):
+        pass
