9db45bb95998aafc57051ac65ae691560e31f403
Hans Hofner <hhofner@gmail.com>
2019-09-24 17:16:59 +0900

    add setpath method and change nexthop method

--- packet.py
+++ packet.py
@@ -1,4 +1,5 @@
 from typing import List, Optional
+import networkx as nx
 
 class Packet:
     current_id = 0
@@ -16,12 +17,31 @@ class Packet:
         self._initial_source = source
         self.size: int       = size
 
+        self.routing_algorithm = None
         self.path: Optional[List[int]] = None
+
+    def set_path(self, graph):
+        if not self.path:
+            if not self.routing_algorithm:
+                self.path = nx.dijkstra_path(graph, self.source, self.destination)
+            else:
+                raise Exception('Have not implemented routing decision from packet yet')
 
     def next_hop(self):
         if not self.path[0] == self.source:
             raise Exception(f'Path problems, first node on path and source IDs do not match!'
                             f'{self.source} =/= {self.path[0]}')
+        return self.path[1]
+
+    def set_current_node(self, current):
+        if current == self.path[1]:
+            self.source = current
+            self.path.pop(0)
+        elif current == self.path[0]:
+            print(f'Warning: Packet has not moved from {self.source}')
+        else:
+            raise Exception(f'Current Node {current} does not match packet {self.id} next hop: {self.path[1]}'
+                            f' or current node {self.path[0]}')
 
     def __repr__(self):
         s = f'Packet({self.id}, init_source:{self._initial_source}, current_source: {self.source}, dest:{self.destination}'
