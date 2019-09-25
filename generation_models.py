b054df6041eed4de161969bee71f3e24f9a9e5e5
Hans Hofner <hhofner@gmail.com>
2019-09-25 22:35:52 +0900

    add packet size and transmission rate usage in basic linear and basic normal

--- generation_models.py
+++ generation_models.py
@@ -2,38 +2,50 @@ import numpy as np
 import packet
 
 def basic_linear_generation(node):
+    packet_size = 8000 #1kB
+    # TODO: SET PACKET PATH WITH SET_PATH() METHOD
     generation_rate = node.config['generation_rate']
+    transmission_rate = node.config['transmission_rate']
     destinations = node.config['destinations']
-    if generation_rate <= 0:
+
+    if transmission_rate <= 0:
+        print(f'Warning: Transmission rate is 0')
         return None
     else:
-        for _ in range(generation_rate):
+        transmit_rate_current = np.random.normal(transmission_rate)
+        for _ in range(transmit_rate_current//packet):
             if not destinations:
+                print(f'Warning: no destinations specified for node {node.id}')
                 return None
             else:
-                rand_int = np.random.randint(len(destinations))
-                node.queue.append(packet.Packet(node.id, destinations[rand_int]))
+                rand_dest = np.random.randint(len(destinations))
+                new_packet = packet.Packet(node.id, destinations[rand_dest], packet_size)
+                new_packet.set_path(node.topology)
+
+                node.queue.append(new_packet)
 
 def basic_normal_generation(node):
+    packet_size = 8000  # 1kB
+    # TODO: SET PACKET PATH WITH SET_PATH() METHOD
     generation_rate = node.config['generation_rate']
+    transmission_rate = node.config['transmission_rate']
     destinations = node.config['destinations']
-    if generation_rate <= 0:
+
+    if transmission_rate <= 0:
+        print(f'Warning: Transmission rate is 0')
         return None
     else:
-        if 'standard_dev' in node.config.keys():
-            scale = node.config['standard_dev']
-        else:
-            scale = int(generation_rate/4)
-
-        packet_rate = int(np.random.normal(generation_rate, scale))
-        packet_rate = 0 if packet_rate < 0 else packet_rate
-
-        for _ in range(packet_rate):
+        transmit_rate_current = np.random.normal(transmission_rate)
+        for _ in range(int(transmit_rate_current // packet_size)):
             if not destinations:
+                print(f'Warning: no destinations specified for node {node.id}')
                 return None
             else:
-                rand_int = np.random.randint(len(destinations))
-                node.queue.append(packet.Packet(node.id, destinations[rand_int]))
+                rand_dest = np.random.randint(len(destinations))
+                new_packet = packet.Packet(node.id, destinations[rand_dest], packet_size)
+                new_packet.set_path(node.topology)
+
+                node.queue.append(new_packet)
 
 def time_experimental_generation(node):
     generation_rate = node.config['generation_rate']
@@ -45,10 +57,13 @@ def time_experimental_generation(node):
         if node.time == 100:
             for _ in range(generation_rate*20):
                 if not destinations:
+                    print(f'Warning: No destinations available for node {node.id}')
                     return None
                 else:
                     rand_int = np.random.randint(len(destinations))
-                    node.queue.append(packet.Packet(node.id, destinations[rand_int], packet_size))
+                    new_packet = packet.Packet(node.id, destinations[rand_int], packet_size)
+                    new_packet.set_path(node.topology)
+                    node.queue.append(new_packet)
         else:
             x = int(np.random.normal(generation_rate, 1))
             for _ in range(x):
@@ -56,4 +71,6 @@ def time_experimental_generation(node):
                     return None
                 else:
                     rand_int = np.random.randint(len(destinations))
-                    node.queue.append(packet.Packet(node.id, destinations[rand_int], packet_size))
+                    new_packet = packet.Packet(node.id, destinations[rand_int], packet_size)
+                    new_packet.set_path(node.topology)
+                    node.queue.append(new_packet)
