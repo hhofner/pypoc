7125d58976d7bea53b5b2234fade526d388b7ed4
Hans Hofner <hhofner@gmail.com>
2019-12-26 22:14:22 +0900

    add linear topology

--- topology.py
+++ topology.py
@@ -5,8 +5,8 @@ from node import Packet, Node, VaryingTransmitNode, VaryingRelayNode, MovingNode
 # TODO: change to allow any connection between number of relays
 def grid(src_count=4, relay_count=8, dest_count=4, bandwidth=1e3):
     src_nodes = [VaryingTransmitNode(0, 1, None, 500, 125) for _ in range(src_count)]
-    relay_nodes_src_side = [RestrictedNode(1, 1, None, 500, 625, 1e3) for _ in range(int(relay_count/2))]
-    relay_nodes_dest_side = [RestrictedNode(1, 1, None, 500, 625, 1e3) for _ in range(int(relay_count/2))]
+    relay_nodes_src_side = [RestrictedNode(1, 1, None, 500, 625, 20e3) for _ in range(int(relay_count/2))]
+    relay_nodes_dest_side = [RestrictedNode(1, 1, None, 500, 625, 20e3) for _ in range(int(relay_count/2))]
     dest_nodes = [MovingNode(2, 1, None) for _ in range(dest_count)]
 
     c1 = []; c2 = []
@@ -38,10 +38,10 @@ def grid(src_count=4, relay_count=8, dest_count=4, bandwidth=1e3):
     return c1 + c2 + c3
 
 def sagin(src_count=4, uav_count=4, sat_count=2, dest_count=4, bandwidth=1e3):
-    src_nodes = [VaryingTransmitNode(0, 1, None, 500, 125) for _ in range(src_count)]
-    uav_src_side = [VaryingRelayNode(1, 1, None, 500, 625) for _ in range(uav_count)]
-    sat_nodes = [VaryingRelayNode(1, 1, None, 500, 625) for _ in range(sat_count)]
-    uav_dest_side = [VaryingRelayNode(1, 1, None, 500, 625) for _ in range(uav_count)]
+    src_nodes = [VaryingTransmitNode(0, 1, None, 500, 500) for _ in range(src_count)]
+    uav_src_side = [RestrictedNode(1, 1, None, 500, 625, 3e3) for _ in range(uav_count)]
+    sat_nodes = [RestrictedNode(1, 1, None, 500, 625, 6e3) for _ in range(sat_count)]
+    uav_dest_side = [RestrictedNode(1, 1, None, 500, 625, 3e3) for _ in range(uav_count)]
     dest_nodes = [MovingNode(2, 1, None) for _ in range(dest_count)]
 
     # Src to UAV
@@ -65,10 +65,22 @@ def sagin(src_count=4, uav_count=4, sat_count=2, dest_count=4, bandwidth=1e3):
         for rel1 in uav_dest_side for rel2 in sat_nodes[1:]]
 
     # sat to sat
-    c6 = [(rel1, rel2, {'Bandwidth': bandwidth, 'Channel': 0})
+    c6 = [(rel1, rel2, {'Bandwidth': 2 * bandwidth, 'Channel': 0})
         for rel1 in sat_nodes for rel2 in sat_nodes]
 
     return c1 + c2 + c3 + c4 + c5 + c6
+
+def linear(nodes=2, bandwidth=1e3, src_count=None):
+    '''
+    Create a "linear" topology for testing purposes. Default is
+    source and destination node.
+    '''
+    if nodes == 2:
+        src_node = VaryingTransmitNode(0, 1, None, 500, 500)
+        dest_node = MovingNode(2, 1, None)
+        return [(src_node, dest_node, {'Bandwidth': bandwidth, 'Channel': 0})]
+    if nodes == 3:
+        pass
 
 ###################################################################################################
 # Drawing Helper Methods #
