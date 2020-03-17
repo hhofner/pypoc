fa6604bbc9b0a0534f6160615031b822e33dd659
Hans Hofner <hhofner@gmail.com>
2020-03-17 14:13:52 +0900

    some docstrings, add setup.ini

--- pypoc/plotter.py
+++ pypoc/plotter.py
@@ -1,5 +1,5 @@
 '''
-Plotting module. Also to be able to be run from the command 
+Plotting module. Also to be able to be run from the command
 line passing in Pickle files as arguments.
 '''
 
@@ -8,4 +8,6 @@ __author__ = 'Hans Hofner'
 import matplotlib.pyplot
 import seaborn as sns
 
-# TODO: Pickle Argument Parsing
+sns.set()
+sns.set_style('whitegrid')
+
