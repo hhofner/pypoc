1c673ca5d25c27ab4a23698fb653c715fc8e03b5
Hans Hofner <hhofner@gmail.com>
2020-03-24 14:46:21 +0900

    now able to run pypoc as module

--- pypoc/__main__.py
+++ pypoc/__main__.py
@@ -2,23 +2,12 @@
 Main entry script for PyPoc. Reads the config and 
 creates & runs a Network.
 '''
+import network
+import toml
 
 __author__ = 'Hans Hofner'
-import configparser
 
-import network
-import topology
-import networkplotter
+configuration = toml.load('config.toml')
 
-# Read Configuration File
-# TODO: Config reading and passing arguments
-from configparser import ConfigParser
-parser = ConfigParser()
-parser.read('setup.ini')
-
-network = network.PyPocNetwork()
-kwargs = {'threshold_value': 62500}
-network.run_network(minutes=15, structure=topology.linear, 
-                    bandwidth=1e3, src_node_count=2,
-                    relay_node_count=None, relay_node_count2=None, 
-                    dest_node_count=None, packet_size=500, **kwargs)
+new = network.PyPocNetwork()
+new.run_network_with(configuration)
