eef4eef68971e626be3201b8d259d61f7689ec29
Hans Hofner <hhofner@gmail.com>
2020-03-24 16:42:19 +0900

    add skeleton to arg parsing

--- pypoc/__main__.py
+++ pypoc/__main__.py
@@ -4,8 +4,14 @@ creates & runs a Network.
 '''
 import network
 import toml
+import argparse
 
 __author__ = 'Hans Hofner'
+
+parser = argparse.ArgumentParser()
+parser.parse_args()
+
+parser.add_argument('-p', '--plot', help='Plot most recent simulation run or passed .csv file.')  #TODO:
 
 configuration = toml.load('config.toml')
 
