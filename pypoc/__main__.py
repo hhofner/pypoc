11d28df332640213e9f36a120a7ca95493b3201a
Hans Hofner <hhofner@gmail.com>
2020-03-26 18:55:16 +0900

    more possible args

--- pypoc/__main__.py
+++ pypoc/__main__.py
@@ -11,7 +11,8 @@ __author__ = 'Hans Hofner'
 parser = argparse.ArgumentParser()
 parser.parse_args()
 
-parser.add_argument('-p', '--plot', help='Plot most recent simulation run or passed .csv file.')  #TODO:
+parser.add_argument('--plot', help='Plot most recent simulation run or passed .csv file.')  #TODO:
+parser.add_argument('--title', type=str, help='Title of simulation run.')
 
 configuration = toml.load('config.toml')
 
