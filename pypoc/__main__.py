8eb46f556b93004754f6882dfccd5458fe9fe11c
Hans Hofner <hhofner@gmail.com>
2020-04-03 15:13:27 +0900

    added create config copy and move to data file

--- pypoc/__main__.py
+++ pypoc/__main__.py
@@ -6,6 +6,9 @@ import network
 import toml
 import argparse
 import plotter
+import shutil
+from pathlib import Path
+from datetime import datetime, timedelta
 
 __author__ = 'Hans Hofner'
 
@@ -15,6 +18,8 @@ parser.add_argument('--plot', action='store_true', help='Plot most recent simula
 parser.add_argument('--all', action='store_true', help='Plot all stats.')
 parser.add_argument('--queue', action='store_true', help='Plot analysis of number of packets in the queue.')
 parser.add_argument('--packets', action='store_true', help='Plot generated/arrived/dropped packets bar chart.')
+parser.add_argument('--throughput', action='store_true', help='Plot the throughput of file(s).')
+parser.add_argument('--progression', default=False, action='store_true')
 parser.add_argument('--datafile', nargs='*', help='Data files to plot.')
 
 parser.add_argument('--run', action='store_true', help='Title of simulation run.')
@@ -25,8 +30,15 @@ args = parser.parse_args()
 configuration = toml.load(args.config)
 
 if args.run:
+    # Copy file
+    shutil.copyfile(args.config, f'./simulation_data/{configuration["title"]}_{datetime.now().strftime("%d%b%y_%H_%M_%S")}_config.toml')
+    # Create simulation data file
+    data_filename = f'{configuration["title"]}_{datetime.now().strftime("%d%b%y_%H_%M_%S")}.csv'
+    Path('./simulation_data/'+data_filename).touch()
+
     new = network.PyPocNetwork()
-    new.run_network_with(configuration)
+
+    new.run_network_with(configuration, **{'filename': data_filename})
 elif args.plot:
     # TODO: Distribute plotting actions -- if more than one (other than all) are speicifed, plot them.
     if args.all:
@@ -41,3 +53,8 @@ elif args.plot:
             plotter.plot_queue_simple(more_filepaths=args.datafile)
         else:
             plotter.plot_queue_simple()
+    elif args.throughput:
+        if args.datafile:
+            plotter.plot_throughput_simple(more_filepaths=args.datafile, progression_view=args.progression)
+        else:
+            plotter.plot_throughput_simple()
