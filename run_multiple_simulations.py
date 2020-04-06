934ed9ea3dc1b2e75d7021ef25248006dc2229e0
Hans Hofner <hhofner@gmail.com>
2020-04-06 20:56:28 +0900

    cute changes

--- run_multiple_simulations.py
+++ run_multiple_simulations.py
@@ -1,6 +1,7 @@
 import pypoc
 import toml
 import os
+import time
 
 NUMBER_OF_SOURCE_NODES = list(range(15, 150))
 
@@ -8,12 +9,13 @@ print("Loading TOML file")
 config = toml.load('config.toml')
 
 for count in NUMBER_OF_SOURCE_NODES:
-    print(f"Editing TOML file from {config['nodes']['src-nodes']['count']} --> {count}")
+    print(f"Editing Source Count from {config['nodes']['src-nodes']['count']} -- to --> {count}")
     config["nodes"]["src-nodes"]["count"] = count
     f=open('config.toml', mode='w')
     toml.dump(config, f)
-    print("Saved to file!")
-    print("Now running...")
+    print("~~~~ Saved to file! ~~~~")
+    time.sleep(0.5)  # IDK why
+    print("...Now running...")
     try:
         os.system("python pypoc --run")
     except:
