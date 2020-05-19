57e0b21e79d1dbfd19f03522d233439f17f0b0f9
Hans Hofner <hhofner@gmail.com>
2020-05-19 21:14:14 +0900

    change multiple simulation script

--- run_multiple_simulations.py
+++ run_multiple_simulations.py
@@ -6,35 +6,34 @@ import argparse
 import shutil
 import time
 
-NUMBER_OF_SOURCE_NODES = list(range(5, 10))
+NUMBER_OF_SOURCE_NODES = list(range(12, 18))
 SIMULATION_RUN_NAME = 'DYNAMIC'
 
-if __name__ == "__main__":
-    config_files = []
-    print(f'{"$"*30} Creating Config Files {"$"*30}')
-    time.sleep(5)
-    for num_src_nodes in NUMBER_OF_SOURCE_NODES:
-        # Copy file
-        new_config_name = f'config_{num_src_nodes}.toml'
-        shutil.copyfile('config.toml', new_config_name)
-        print(f'Creating copy of config: {new_config_name}')
-        new_config = toml.load(new_config_name)
-        new_config['nodes']['src-nodes']['count'] = num_src_nodes
-        new_config['title'] = SIMULATION_RUN_NAME + f'_{num_src_nodes}'
+config_files = []
+print(f'{"$"*30} Creating Config Files {"$"*30}')
+time.sleep(5)
+for num_src_nodes in NUMBER_OF_SOURCE_NODES:
+    # Copy file
+    new_config_name = f'config_{num_src_nodes}.toml'
+    shutil.copyfile('config.toml', new_config_name)
+    print(f'Creating copy of config: {new_config_name}')
+    new_config = toml.load(new_config_name)
+    new_config['nodes']['src-nodes']['count'] = num_src_nodes
+    new_config['title'] = SIMULATION_RUN_NAME + f'_{num_src_nodes}'
 
-        # Save it back to file
-        f = open(new_config_name, mode='w')
-        toml.dump(new_config, f)
-        f.close()
-        config_files.append(new_config_name)
+    # Save it back to file
+    f = open(new_config_name, mode='w')
+    toml.dump(new_config, f)
+    f.close()
+    config_files.append(new_config_name)
 
-    for config_file in config_files:
-        print(f'{"$"*30} Running Simulation with {config_file} {"$"*30}')
-        try:
-            os.system(f"python pypoc --run --config {config_file}")
-        except:
-            print('Couldnt run for config {config_file}')
-            raise
-        else:
-            print('Done! Removing {config_file}')
-            os.remove(config_file)
+for config_file in config_files:
+    print(f'{"$"*30} Running Simulation with {config_file} {"$"*30}')
+    try:
+        os.system(f"python pypoc --run --config {config_file}")
+    except:
+        print('Couldnt run for config {config_file}')
+        raise
+    else:
+        print('Done! Removing {config_file}')
+        os.remove(config_file)
