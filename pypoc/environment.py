7cb723746fc437cb877708c7a1be4e5425010899
Hans Hofner <hhofner@gmail.com>
2020-06-08 20:41:57 +0900

    y

--- pypoc/environment.py
+++ pypoc/environment.py
@@ -3,4 +3,10 @@ This file will define the environment in abstract parameters such as
 tick time.
 '''
 
+class Default:
+    def __init__(self, network):
+        self.tic
+        self.network = network
 
+    def run(self):
+        self.network.run()
