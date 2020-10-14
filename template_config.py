a6eead5f52af342c678bf91af6a9688181ead983
Hans Hofner <hhofner@gmail.com>
2020-10-14 23:22:17 +0900

    move stuff, rename stuff, push stuff

---
+++ template_config.py
@@ -0,0 +1,21 @@
+intro_model = dict (
+        capacity = 500,
+        max_neighbors = 4,
+        gamma = 0.798,
+        batch_size = 30,
+        eps_start = 0.8,
+        eps_end = 0.15,
+        eps_decay = 200,
+        net_tick = 300
+)
+
+surgeon_model = dict (
+        capacity = 500,
+        max_neighbors = 4,
+        gamma = 0.798,
+        batch_size = 30,
+        eps_start = 0.8,
+        eps_end = 0.25,
+        eps_decay = 400,
+        net_tick = 300
+)
