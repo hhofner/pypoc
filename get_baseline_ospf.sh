49bd19e34a3c7f3d2471e75e2bc34913e7b40c48
Hans Hofner <hhofner@gmail.com>
2020-10-14 23:05:14 +0900

    remove useless files

--- get_baseline_ospf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Run a single simulation for every configuration in the config directory
-for config_file in /config/*.toml; do
-done
-
-# Run increasing source node scripts for every configuration in the config directory
-
-# Run increasing 
